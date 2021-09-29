e4eec21be37b2446fe6af280a833216df2b75ffb
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-09-29 17:03:09 +0200

    server ready to run

---
+++ index.js
@@ -0,0 +1,9 @@
+const PORT = 8000;
+
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+
+app.listen(PORT, () => console.log(`the server is running on ${PORT}`));
