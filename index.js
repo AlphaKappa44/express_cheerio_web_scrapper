51c9c064deac19cba3b3daceda845d3ad885ad04
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-09-29 17:49:59 +0200

    cheerio to select title and url from articles

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ const cheerio = require('cheerio');
 
 const app = express();
 
-// Use axios to pick html
+// Use axios to extract html
 const url = 'https://www.theguardian.com/uk';
 axios(url)
     .then(response => {
         const html = response.data;
-        console.log(html);
-    });
+        // console.log(html);
+
+        // use cheerio to pick one element
+        const $= cheerio.load(html);
+        // build an array
+        const articles = [];
+
+        $('.fc-item__title', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        });
+        console.log(articles);
+    }).catch(err => console.log(err));
 
 app.listen(PORT, () => console.log(`the server is running on ${PORT}`));
