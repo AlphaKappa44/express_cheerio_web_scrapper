0872ee1779455a7374559ae098fd8b69085adb1b
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-09-29 17:16:26 +0200

    use axios to extract html from website

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+// Use axios to pick html
+const url = 'https://www.theguardian.com/uk';
+axios(url)
+    .then(response => {
+        const html = response.data;
+        console.log(html);
+    });
+
 app.listen(PORT, () => console.log(`the server is running on ${PORT}`));
